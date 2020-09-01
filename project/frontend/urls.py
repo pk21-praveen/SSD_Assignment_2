76a37468d9114302edce30c1683262e67efe6dc1
Praveenkumar Srikanthan <it17137324@my.sliit.lk>
2020-09-01 22:55:09 +0530

    final

--- project/frontend/urls.py
+++ project/frontend/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path('', views.FileFieldView.as_view()),
+    path('', views.index),
+
 ]
--- project/frontend/views.py
+++ project/frontend/views.py
@@ -1,20 +1,5 @@
-from django.views.generic.edit import FormView
-from .forms import FileFieldForm
+from django.shortcuts import render
 
-
-class FileFieldView(FormView):
-    form_class = FileFieldForm
-
-    def post(self, request):
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
-        files = request.FILES.getlist('file')
-        if form.is_valid():
-            for f in files:
-                with open(Path(settings.MEDIA_ROOT + "/" + f.name).resolve(), 'wb+') as destination:
-                    for chunk in f.chunks():
-                        destination.write(chunk)
-
-            return JsonResponse({'form': True})
-        else:
-            return JsonResponse({'form': False})
+# Create your views here.
+def index(request):
+    return render(request, 'frontend/upload.html')
