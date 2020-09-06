0afccc679f5dff9bed124977f716fa27f480d21e
Praveenkumar Srikanthan <it17137324@my.sliit.lk>
2020-09-06 18:49:55 +0530

    refactored

--- project/frontend/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class ImageModel(models.Model):
-    image = models.ImageField("drag and drop an image here",
-                              upload_to="images/drag-and-drop/",
-                              height_field=None,
-                              width_field=None, max_length=None)
