6275512d6ea89370c5d922081aa81f06f912bf75
Praveenkumar Srikanthan <praveenkumar.srikanthan@ifsworld.com>
2020-10-06 17:49:15 +0530

    code refactoring

--- FileUploader/server.js
+++ FileUploader/server.js
@@ -1,133 +1,157 @@
+// Importing necessary libraries
 const express = require("express");
 const { google } = require("googleapis");
 const fs = require("fs");
 const multer = require("multer");
 
-const oAuth2Client = require('./credentials/oauth2Client');
+// Importing client credentials
+const clientAuth = require('./credentials/clientAuth');
 
+// express app initialization
 const app = express();
+
+// ststic folder path declaration
 app.use(express.static("public"));
+
+// set view template
 app.set("view engine", "ejs");
 
+// declare required variables 
 let username, picture, authed = false;
 
+// define google api scopes
 const scopes = [
   'https://www.googleapis.com/auth/userinfo.profile',
   'https://www.googleapis.com/auth/drive.file'
 ];
 
+// initialize multer middleware for handling multipart form-data
 let Storage = multer.diskStorage({
   destination: function (req, file, callback) {
+    // local storage file destination
     callback(null, "./drive");
   },
   filename: function (req, file, callback) {
-    callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+    // setting a file name
+    callback(null, file.originalname + "-" + Date.now());
   },
 });
 
-let upload = multer({ storage: Storage }).single("file");
+// initialize multer singleFileUploader function
+let singleFileUploader = multer({ storage: Storage }).single("file");
 
+// landing page api
 app.get("/", (req, res) => {
-  if (!authed) {
-    // Generate an OAuth URL and redirect there
-    var url = oAuth2Client.generateAuthUrl({
-      access_type: "offline",
-      scope: scopes,
-    });
 
+  // if already logged
+  if (!authed) {
+    // generate oauth url 
+    var url = clientAuth.generateAuthUrl({ access_type: "offline", scope: scopes });
+
+    // redirect to index.ejs view with url
     res.render("index", { url: url });
-    
+
   } else {
-    var oauth2 = google.oauth2({
-      version: 'v2',
-      auth: oAuth2Client
-    });
-    oauth2.userinfo.get(function (err, response) {
+
+    // initialize google oauth2 api
+    var googleOAuth2 = google.oauth2({ auth: clientAuth, version: 'v2' });
+
+    // get user info
+    googleOAuth2.userinfo.get(function (err, response) {
+
       if (err) {
+        // handle error
         console.log(err);
       } else {
-        console.log(response.data);
-
+        // assign userinfo to local variables
         username = response.data.name
         picture = response.data.picture
 
-        res.render("upload", {
-          username: username,
-          picture: picture,
-          success: false
-        });
+        // pass the userinfo to upload.ejs view
+        res.render("upload", { username: username, picture: picture, isUploaded: false });
       }
+
     });
   }
 });
 
-app.post("/upload", (req, res) => {
-  upload(req, res, function (err) {
+// file upload api
+app.post("/uploadFile", (req, res) => {
+  // uploading file using multer
+  singleFileUploader(req, res, function (err) {
     if (err) {
-      console.log(err);
-      return res.end("Something went wrong");
+      // handle error
+      return res.end("Error: Unable to upload file");
     } else {
-      console.log(req.file.path);
+      // initialize google drive api
+      const drive = google.drive({ version: "v3", auth: clientAuth });
 
-      const drive = google.drive({ version: "v3", auth: oAuth2Client });
-
-      const fileMetadata = {
-        name: req.file.filename,
-      };
-
-      const media = {
-        mimeType: req.file.mimetype,
-        body: fs.createReadStream(req.file.path),
-      };
-
-      drive.files.create(
-        {
-          resource: fileMetadata,
-          media: media,
-          fields: "id",
+      // set file data
+      let fileData = {
+        fields: "id",
+        media: {
+          mimeType: req.file.mimetype,
+          body: fs.createReadStream(req.file.path),
         },
+        resource: {
+          name: req.file.filename,
+        }
+      };
+
+      // upload files to drive
+      drive.files.create(fileData,
         (err, file) => {
           if (err) {
-            // Handle error
+            // handle error
             console.error(err);
           } else {
+            // remove local file
             fs.unlinkSync(req.file.path)
-            res.render("upload", { 
-              username: username,
-              picture: picture,
-              success: true
-            });            
+
+            // return respnse to upload.ejs view
+            res.render("upload", { username: username, picture: picture, isUploaded: true });
           }
         }
       );
+
     }
   });
 });
 
-app.get("/auth/callback", function (req, res) {
+// goole auth callback api
+app.get("/auth/google/callback", function (req, res) {
   const code = req.query.code;
   if (code) {
-    // Get an access token based on our OAuth code
-    oAuth2Client.getToken(code, function (err, tokens) {
+    // Get an access token from the google based on oauth code
+    clientAuth.getToken(code, function (err, tokens) {
+
       if (err) {
-        console.log("Error authenticating");
-        console.log(err);
+        // handle error
+        console.log("Error: Failed to authenticate");
       } else {
-        console.log("Successfully authenticated");
-        console.log(tokens)
-        oAuth2Client.setCredentials(tokens);
+        // set client credentials through access token
+        clientAuth.setCredentials(tokens);
+
+        // enable true for auth variable
         authed = true;
+
+        // redirect to landing page
         res.redirect("/");
       }
     });
   }
 });
 
+// logout api
 app.get('/logout', (req, res) => {
+  // enable false for auth variable
   authed = false
+
+  // redirect to landing page
   res.redirect('/')
 })
 
+// app listening port
 app.listen(3000, () => {
   console.log("Listening on Port 3000");
 });
